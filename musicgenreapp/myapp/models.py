3ba13d8040ed9947ed18d5441c33b5aac10807a7
Phumiwit phumchon <68888298+phumiwit@users.noreply.github.com>
2023-09-05 16:07:35 +0700

    Update models.py

--- musicgenreapp/myapp/models.py
+++ musicgenreapp/myapp/models.py
@@ -8,9 +8,9 @@ class Userdata(models.Model):
     UserName = models.CharField(max_length = 20)
     FirstName = models.CharField(max_length = 20)
     LastName = models.CharField(max_length = 20)
-    age = models.IntegerField()
-    email = models.EmailField(max_length=254)
-    date = models.DateField(auto_now_add = True)
+    Age = models.IntegerField()
+    Email = models.EmailField(max_length=254)
+    Date = models.DateField(auto_now_add = True)
     
     def __str__(self):
         return self.FirstName + "," + self.LastName
@@ -19,25 +19,25 @@ class Audio(models.Model):
     UserName = models.ForeignKey(User, on_delete=models.CASCADE)
     Filename = models.CharField(max_length=254)
     Genre = models.CharField(max_length=100, null=True, blank=True)
-    top3_genre = models.CharField(max_length=255, null=True, blank=True)
+    Top3_Genre = models.CharField(max_length=255, null=True, blank=True)
     Value = Value = JSONField()
     
 
 class Collection(models.Model):
     UserName = models.ForeignKey(User, on_delete=models.CASCADE)
-    filename = models.CharField(max_length=255)
-    genre = models.CharField(max_length=255)
-    top3_genre = models.CharField(max_length=255, null=True, blank=True)
-    audio_id = models.CharField(max_length=255)
-    collectionname = models.CharField(max_length=255, null=True, blank=True)
+    FileName = models.CharField(max_length=255)
+    Genre = models.CharField(max_length=255)
+    Top3_Genre = models.CharField(max_length=255, null=True, blank=True)
+    Audio_Id = models.CharField(max_length=255)
+    CollectionName = models.CharField(max_length=255, null=True, blank=True)
     
     
 class File(models.Model):
     UserName = models.ForeignKey(User, on_delete=models.CASCADE)
-    filename = models.CharField(max_length=250)
-    fileurl = models.FileField(upload_to='uploads/')
+    FileName = models.CharField(max_length=250)
+    FileUrl = models.FileField(upload_to='uploads/')
     
 
 class Valueforchart(models.Model):
-    genre = models.CharField(max_length=255, null=True, blank=True)
-    age = models.IntegerField(null=True, blank=True)
+    Genre = models.CharField(max_length=255, null=True, blank=True)
+    Age = models.IntegerField(null=True, blank=True)
